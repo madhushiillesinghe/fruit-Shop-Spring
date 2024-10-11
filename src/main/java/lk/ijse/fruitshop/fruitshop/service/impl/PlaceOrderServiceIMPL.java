0f1b786082083b07d9d7bb60d704813e65636d3e
madhuille11 <madhushiillesinghe225@gmail.com>
2024-10-11 09:57:32 +0530

    transaction completed

--- src/main/java/lk/ijse/fruitshop/fruitshop/service/impl/PlaceOrderServiceIMPL.java
+++ src/main/java/lk/ijse/fruitshop/fruitshop/service/impl/PlaceOrderServiceIMPL.java
@@ -7,6 +7,7 @@ import lk.ijse.fruitshop.fruitshop.dto.impl.ItemDTO;
 import lk.ijse.fruitshop.fruitshop.dto.impl.OrderDTO;
 import lk.ijse.fruitshop.fruitshop.dto.impl.OrderDetailDTO;
 import lk.ijse.fruitshop.fruitshop.embedded.OrderDetailPrimaryKey;
+import lk.ijse.fruitshop.fruitshop.entity.impl.ItemEntity;
 import lk.ijse.fruitshop.fruitshop.entity.impl.OrderDetailEntity;
 import lk.ijse.fruitshop.fruitshop.entity.impl.OrderEntity;
 import lk.ijse.fruitshop.fruitshop.exception.DataPersistFailedException;
@@ -18,6 +19,8 @@ import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 public class PlaceOrderServiceIMPL implements PlaceOrderService {
@@ -26,10 +29,11 @@ public class PlaceOrderServiceIMPL implements PlaceOrderService {
     @Autowired
     private OrderDetailDAO orderDetailDAO;
     @Autowired
+    private  ItemDAO itemDAO;
+    @Autowired
     private Mapping mapping;
     @Override
     public void saveOrder(OrderDTO orderDto) {
-
         orderDto.setOrderId(AppUtil.createOrderId());
         OrderEntity orderEntity = mapping.convertToOrderEntity(orderDto);
         orderEntity.setOrder_id(orderDto.getOrderId());
@@ -45,25 +49,24 @@ public class PlaceOrderServiceIMPL implements PlaceOrderService {
                 orderDetailDTO.setUnitPrice(itemDTO.getPrice());
                 orderDetailDTO.setItemCode(itemDTO.getCode());
 
-
-
                 OrderDetailPrimaryKey orderDetailPrimaryKey=new OrderDetailPrimaryKey(orderDto.getOrderId(),itemDTO.getCode());
                 OrderDetailEntity orderDetailEntity = mapping.convertToOrderDetailEntity(orderDetailDTO);
-
-                System.out.println(orderDetailEntity+"As service");
-
 
                 orderDetailEntity.setOrderDetailPrimaryKey(orderDetailPrimaryKey);
                 orderDetailEntity.setUnit_price(itemDTO.getPrice());
                 orderDetailEntity.setQty(itemDTO.getQuantity());
                 orderDetailEntity.setUnit_price(itemDTO.getPrice());
 
-                System.out.println(orderDetailEntity+"As service");
 
                 OrderDetailEntity saveOrderDetail=orderDetailDAO.save(orderDetailEntity);
                 if (saveOrderDetail == null && saveOrderDetail.getOrder().getOrder_id() == null) {
                     throw new DataPersistFailedException();
-            }
+            }else {
+                        ItemEntity itemEntity=mapping.convertToItemEntity(itemDTO);
+                        Optional<ItemEntity> item=itemDAO.findById((itemDTO.getCode()));
+                        itemEntity.setQuantity((item.get().getQuantity())-itemDTO.getQuantity());
+                        itemDAO.save(itemEntity);
+                }
 
         }
         }
